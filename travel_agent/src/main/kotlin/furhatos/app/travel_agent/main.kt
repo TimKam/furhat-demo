8fe40c46ba81ac4698a46f75e307715336386a27
Timotheus Kampik <timotheus.kampik@gmail.com>
2018-12-02 20:17:58 +0100

    fix media type

--- travel_agent/src/main/kotlin/furhatos/app/travel_agent/main.kt
+++ travel_agent/src/main/kotlin/furhatos/app/travel_agent/main.kt
@@ -21,7 +21,7 @@ class Travel_agentSkill : Skill() {
 fun main(args: Array<String>) {
     val client = OkHttpClient()
 
-    val mediaType = OkMediaType.parse("application/x-www-form-urlencoded")
+    val mediaType = OkMediaType.parse("multipart/form-data")
     val body = RequestBody.create(mediaType, "inpPointFr_ajax=Ume%25E5%2B%25D6stermalmsgatan%257C25340%257C0&inpPointTo_ajax=Ume%25E5%2BVasaplan%257C25332%257C0&inpPointInterm_ajax=&selRegionFr=741&inpPointFr=&optTypeFr=0&inpPointTo=&optTypeTo=0&inpPointInterm=&selDirection=0&inpTime=21%253A16&inpDate=2018-12-01&optReturn=0&selDirection2=0&inpTime2=00%253A10&inpDate2=2018-12-02&trafficmask=2&selChangeTime=0&selWalkSpeed=0&selPriority=0&cmdAction=search&EU_Spirit=False&TNSource=UMEA&SupportsScript=True&Language=se&VerNo=&Source=querypage_adv&MapParams=")
     val request = Request.Builder()
             .url("https://reseplanerare.fskab.se/umea/v2/querypage_adv.aspx")
@@ -39,6 +39,5 @@ fun main(args: Array<String>) {
     System.out.println(response.message())
     System.out.println(response.body())
     System.out.println(response.body()!!.contentType())
-    System.out.println(response.body()!!.byteStream().)
-    Skill.main(args)
+    System.out.println(response.body()?.string())
 }
