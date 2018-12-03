be08613b524d9228e23f4a867c192ad57c7df54e
Tobbis <tobias_sundqvist@yahoo.se>
2018-12-03 11:20:52 +0100

    Update users.kt

--- travel_agent/src/main/kotlin/furhatos/app/travel_agent/users.kt
+++ travel_agent/src/main/kotlin/furhatos/app/travel_agent/users.kt
@@ -1,2 +1,12 @@
 package furhatos.app.travel_agent.flow
 
+import furhatos.app.travel_agent.nlu.OrderBusIntent
+import furhatos.flow.kotlin.NullSafeUserDataDelegate
+import furhatos.records.User
+import furhatos.util.Language
+
+// Associate an order to a user
+val User.order by NullSafeUserDataDelegate { OrderBusIntent() }
+
+//var GlobalLanguage = Language.ENGLISH_US
+var GlobalLanguage = Language.SWEDISH
