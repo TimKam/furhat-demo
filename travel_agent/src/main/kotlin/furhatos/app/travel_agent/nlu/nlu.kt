50f574a0961a2c2d899847645d155a435df804b6
Tobbis <tobias_sundqvist@yahoo.se>
2018-12-03 11:20:09 +0100

    Update nlu.kt

--- travel_agent/src/main/kotlin/furhatos/app/travel_agent/nlu/nlu.kt
+++ travel_agent/src/main/kotlin/furhatos/app/travel_agent/nlu/nlu.kt
@@ -1,1 +1,16 @@
 package furhatos.app.travel_agent.nlu
+
+import furhatos.nlu.EnumEntity
+import furhatos.util.Language
+
+class Place : EnumEntity() {
+
+    override fun getEnum(lang: Language): List<String> {
+        return listOf("vasaplan", "guitars")
+    }
+
+    // Method overridden to produce a spoken utterance of the place
+    override fun toText(lang: Language): String {
+        return generate(lang,"to $value");
+    }
+}
