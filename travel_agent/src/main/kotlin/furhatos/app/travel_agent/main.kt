fd6d9d9d673b8986c637cbbe15b6027861960884
Timotheus Kampik <timotheus.kampik@gmail.com>
2018-12-03 10:34:04 +0100

    fix address ID issue

--- travel_agent/src/main/kotlin/furhatos/app/travel_agent/main.kt
+++ travel_agent/src/main/kotlin/furhatos/app/travel_agent/main.kt
@@ -12,6 +12,7 @@ import okhttp3.Response
 import okhttp3.MediaType as OkMediaType
 
 
+val client = OkHttpClient()
 
 class Travel_agentSkill : Skill() {
     override fun start() {
@@ -19,11 +20,48 @@ class Travel_agentSkill : Skill() {
     }
 }
 
-fun main(args: Array<String>) {
+fun getSchedule(startPlace: String, destination: String, startDate: String, startTime: String) {
     val client = OkHttpClient()
 
+    val getStartRequest = Request.Builder()
+            .url("https://reseplanerare.fskab.se/umea/v2/rpajax.aspx?net=UMEA&lang=se&letters=$startPlace")
+            .get()
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .addHeader("Accept-Encoding", "gzip, deflate, br")
+            .addHeader("Cache-Control", "no-cache")
+            .addHeader("Postman-Token", "c692ec64-2f35-4304-b2cc-a9153fe247f2")
+            .build()
+
+    val getStartResponseBody = client.newCall(getStartRequest).execute().body()?.string()
+    val startPlaceId = getStartResponseBody!!
+            .substring(getStartResponseBody.indexOf("|")+1)
+            .substring(0, 5)
+    println("startPlaceId:")
+    println(startPlaceId)
+
+    val getDestinationRequest = Request.Builder()
+            .url("https://reseplanerare.fskab.se/umea/v2/rpajax.aspx?net=UMEA&lang=se&letters=$destination")
+            .get()
+            .addHeader("Content-Type", "application/x-www-form-urlencoded")
+            .addHeader("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8")
+            .addHeader("Accept-Encoding", "gzip, deflate, br")
+            .addHeader("Cache-Control", "no-cache")
+            .addHeader("Postman-Token", "c692ec64-2f35-4304-b2cc-a9153fe247f2")
+            .build()
+
+    val getDestinationResponseBody = client.newCall(getDestinationRequest).execute().body()?.string()
+    var destinationId = getDestinationResponseBody!!
+            .substring(getDestinationResponseBody.indexOf("|")+1)
+            .substring(0, 5)
+
+    println("destinationId:")
+    println(destinationId)
+
+
+    // Example: D6stermalmsgatan, Vasaplan, 2018-12-01, 21%3A16, (second time: 00%3A10)
     val mediaType = OkMediaType.parse("raw")
-    val body = RequestBody.create(mediaType, "inpPointFr_ajax=Ume%E5+%D6stermalmsgatan%7C25340%7C0&inpPointTo_ajax=Ume%E5+Vasaplan%7C25332%7C0&inpPointInterm_ajax=&selRegionFr=741&inpPointFr=&optTypeFr=0&inpPointTo=&optTypeTo=0&inpPointInterm=&selDirection=0&inpTime=21%3A16&inpDate=2018-12-01&optReturn=0&selDirection2=0&inpTime2=00%3A10&inpDate2=2018-12-02&trafficmask=2&selChangeTime=0&selWalkSpeed=0&selPriority=0&cmdAction=search&EU_Spirit=False&TNSource=UMEA&SupportsScript=True&Language=se&VerNo=&Source=querypage_adv&MapParams=")
+    val body = RequestBody.create(mediaType, "inpPointFr_ajax=Ume%E5+%$startPlace%7C$startPlaceId%7C0&inpPointTo_ajax=Ume%E5+$destination%7C$destinationId%7C0&inpPointInterm_ajax=&selRegionFr=741&inpPointFr=&optTypeFr=0&inpPointTo=&optTypeTo=0&inpPointInterm=&selDirection=0&inpTime=$startTime&inpDate=$startDate&optReturn=0&selDirection2=0&inpTime2=$startTime&inpDate2=$startDate&trafficmask=2&selChangeTime=0&selWalkSpeed=0&selPriority=0&cmdAction=search&EU_Spirit=False&TNSource=UMEA&SupportsScript=True&Language=se&VerNo=&Source=querypage_adv&MapParams=")
     val request = Request.Builder()
             .url("https://reseplanerare.fskab.se/umea/v2/querypage_adv.aspx")
             .post(body)
@@ -42,38 +80,47 @@ fun main(args: Array<String>) {
     val schedule = arrayListOf<String>()
     responseLines?.forEachIndexed {
         index, line ->
-            println(line)
-            if(line.contains("rp-date-header")) {
-                val dateLine = line
-                        .substring(line.indexOf(">")+1)
-                        .substring(0, 10)
-                schedule.add(dateLine)
-            } else if (line.contains("summary-timeframe")) {
-                val timesLine = responseLines[index + 1]
-                val startTime = timesLine
-                        .substring(timesLine.indexOf(";")+1)
+        println(line)
+        if(line.contains("rp-date-header")) {
+            val dateLine = line
+                    .substring(line.indexOf(">")+1)
+                    .substring(0, 10)
+            schedule.add(dateLine)
+        } else if (line.contains("summary-timeframe")) {
+            val timesLine = responseLines[index + 1]
+            val startTime = timesLine
+                    .substring(timesLine.indexOf(";")+1)
+                    .substring(0, 5)
+            val endTime = timesLine
+                    .substring(timesLine.indexOf("&raquo;")+8)
+                    .substring(0, 5)
+            schedule.add(startTime)
+            schedule.add(endTime)
+        } else if(line.contains("summary-heading")) {
+            if(line.contains("Restid")) {
+                val travelTimeLine = line
+                        .substring(line.indexOf(">Restid")+8)
                         .substring(0, 5)
-                val endTime = timesLine
-                        .substring(timesLine.indexOf("&raquo;")+8)
-                        .substring(0, 5)
-                schedule.add(startTime)
-                schedule.add(endTime)
-            } else if(line.contains("summary-heading")) {
-                if(line.contains("Restid")) {
-                    val travelTimeLine = line
-                            .substring(line.indexOf(">Restid")+8)
-                            .substring(0, 5)
-                    schedule.add(travelTimeLine)
-                }
-                else {
-                    val switchesLine = line
-                            .substring(line.indexOf("heading'>")+9)
-                            .substring(0, 1)
-                    schedule.add(switchesLine)
-                }
-
+                schedule.add(travelTimeLine)
             }
+            else {
+                println(line)
+                val switchesLine = line
+                        .substring(line.indexOf("heading'>")+9)
+                        .substring(0, 1)
+                schedule.add(switchesLine)
+            }
+
+        }
     }
     println("Schedule:")
     println(schedule)
+
+}
+
+fun main(args: Array<String>) {
+    // "https://reseplanerare.fskab.se/umea/v2/rpajax.aspx?net=UMEA&lang=se&letters=address
+    getSchedule("Östermalmsgatan", "Vasaplan", "2018-12-01", "16:15")
+    Skill.main(args)
+
 }
